00540ff4dcb3e15c2613fac388fd304e79157b2c
Richard Herron <richard.c.herron@icloud.com>
2022-09-01 19:53:30 +0000

    Removed workspace file and github script

--- Notebooks/push2github.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash/
-inotifywait --monitor --timeout 7200 --event modify --include '\/[hlm].*ipynb'  ./Herron ./Lewinson ./McKinney --format "git commit -m 'Auto commit during class' %w && git push" | bash
